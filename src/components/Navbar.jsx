80165d618159642cb410fdaa0ab7f9d113cc3ae8
Arnab Dev <official.arnabdev@gmail.com>
2024-06-09 16:53:34 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className=' container mx-auto px-20 py-8 font-["Neue_Montreal"] flex justify-between items-center' >
         <div className='logo'>
-            <img width="120" height="30" fill="none" src="../../public/logo3.webp"/>
+            <img width="120" height="30" fill="none" src="logo3.webp"/>
 									
         </div>
         <div className=' flex gap-10 flex-wrap'>
