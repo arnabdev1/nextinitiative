8e4211aefbbc6124c74b51c95b3e84b26b10c166
Arnab Dev <official.arnabdev@gmail.com>
2024-06-09 16:58:11 +0600

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,9 +12,9 @@ function About() {
         <div className='p-10 flex flex-wrap w-full gap-5 pt-10 border-t-[1px] mt-20 border-[#b5627e] bg-zinc-900 rounded-[20px]'>
             <div className='w-1/2'>
               <h1 className='text-7xl text-white '>Our Team:</h1>
-              <button className='flex gap-10 items-center px-10 py-6 mt-10 bg-black rounded-full text-white'>
+              <button className='flex gap-10 items-center px-10 py-6 mt-10 bg-[#CDEA68] font-bold rounded-full text-white'>
                   Read More
-                  <div className='w-3 h-3 rounded-full bg-zinc-100'>
+                  <div className='w-3 h-3 rounded-full bg-zinc-900'>
 
                   </div>
               </button>
