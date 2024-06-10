e6df906d4f8626fe628a0858cf0bbd76cda8f044
Arnab Dev <official.arnabdev@gmail.com>
2024-06-10 10:42:43 +0600

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,34 +2,33 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='w-full p-10  bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black '>
-        <h1 className='font-["Neue_Montreal"] text-[4vw] leading-[4.5vw] tracking-tighter '>
+    <div className='team w-full   bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black '>
+        <h1 className='px-10 pt-10 text-bold font-["Neue_Montreal"] text-[5vw] leading-[4.5vw] tracking-tighter '>
             Non-profit Organisaiton | Run by students 
         </h1>
-        <h1>    
+        <h1 className='px-10 font-["Neue_Montreal"] text-[3vw] leading-[4.5vw] tracking-tighter '>    
             Donate to us and help us feed another hungry soul
         </h1> 
-        <div className='p-10 flex flex-wrap w-full gap-5 pt-10 border-t-[1px] mt-20 border-[#b5627e] bg-zinc-900 rounded-[20px]'>
-            <div className='w-1/2'>
-              <h1 className='text-7xl text-white '>Our Team:</h1>
-              <button className='flex gap-10 items-center px-10 py-6 mt-10 bg-[#CDEA68] font-bold rounded-full '>
-                  Read More
+        <div className='px-10 pt-20  flex flex-wrap w-full gap-5  border-t-[1px] mt-10 border-[#b5627e] bg-zinc-900 rounded-[20px]'>
+            <div className='w-full'>
+              <h1 className='text-5xl font-["Neue_Montreal"] font-bold tracking-tight text-white'>Our Team:</h1>
+              <a href="https://forms.gle/uKMd3XEuauNv2vJDA" target="_blank"><button className='flex gap-10 items-center px-10 py-6 mt-10 bg-[#CDEA68] font-bold rounded-full '>
+                  Join now
                   <div className='w-3 h-3 rounded-full bg-zinc-900'>
-
                   </div>
-              </button>
+              </button></a>
             </div>
-            <div className='flex'>
-              <img className='w-1/2' src="arnab.jpg"/>
-              <img className='w-1/2' src="sheo.jpg"/>
+            <div className='flex flex-col md:flex-row justify-center items-center'>
+              <img className='w-[650px] ' src="arnab.jpg"/>
+              <img className='w-[650px]' src="sheo.jpg"/>
             </div>
-            <div className='flex'>
-              <img className='w-1/2' src="shirsho.jpg"/>
-              <img className='w-1/2' src="labib.jpg"/>
+            <div className='flex flex-col md:flex-row justify-center items-center'>
+              <img className='w-[650px] ' src="shirsho.jpg"/>
+              <img className='w-[650px]' src="labib.jpg"/>
             </div>
-            <div className='flex'>
-              <img className='w-1/2' src="joy.jpg"/>
-              <img className='w-1/2' src="abrar.jpg"/>
+            <div className='flex flex-col md:flex-row justify-center items-center'>
+              <img className='w-[650px] ' src="joy.jpg"/>
+              <img className='w-[650px]' src="abrar.jpg"/>
             </div>
         </div>
         
