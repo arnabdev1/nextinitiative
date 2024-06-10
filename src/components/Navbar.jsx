e37a13a671183ca85b7dbf6e515e742f1217d7a1
Arnab Dev <official.arnabdev@gmail.com>
2024-06-10 10:44:06 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
 function Navbar() {
+  const redirectToWebsite = (website) => {
+    window.open(website, '_blank');
+  };
   return (
-    <div className=' container mx-auto px-20 py-8 font-["Neue_Montreal"] flex justify-between items-center' >
+    <div  className='bg-zinc-900 gap-10 container mx-auto px-20 py-8 font-["Neue_Montreal"] flex justify-between items-center' >
         <div className='logo'>
-            <img width="120" height="30" fill="none" src="logo3.webp"/>
+            <img width="400" height="200" fill="none" src="../../public/logo3.webp"/>
 									
         </div>
         <div className=' flex gap-10 flex-wrap'>
-            {["Services","Our Work","About Us","Insights","Contact"].map((item,index)=> 
-            (<a key={index} className={`text-lg font-light `}> {item} </a>
-            ))}
+            <a href="https://www.facebook.com/nextinitiativebd" target="_blank"><button className='text-lg font-light'>About Us</button></a>
+            <a href="https://www.facebook.com/nextinitiativebd/events" target="_blank"><button className='text-lg font-light'>Our Work</button></a>
+            <a href="https://forms.gle/uKMd3XEuauNv2vJDA" target="_blank"><button className='text-lg font-light'>Join us</button></a>
+            <a href="https://www.facebook.com/messages/t/104462571360723/" target="_blank"><button className='text-lg font-light'>Contact</button></a>
         </div>
     </div>
   )
